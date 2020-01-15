f3be7b9b7439ba958ddf2b187cb60b056c3d6ab0
Icha <predrika@multiinti.com>
2020-01-15 16:19:07 +0700

    Initial commit

---
+++ app/src/main/java/com/arie/onlineloan/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.arie.onlineloan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btn_login);
+        TextView tvRegister = findViewById(R.id.txt_register);
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inReg = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(inReg);
+            }
+        });
+    }
+}
