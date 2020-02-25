712fd779f5f657854e451a24c0966b283485de21
tdykr <30433965+tdykr@users.noreply.github.com>
2020-02-25 20:46:36 +0700

    add SSL Certf

--- app/src/main/java/com/arie/onlineloan/LoginActivity.java
+++ app/src/main/java/com/arie/onlineloan/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.arie.onlineloan;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,7 +24,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -41,6 +51,8 @@ public class LoginActivity extends AppCompatActivity {
         Button btnLogin = findViewById(R.id.btn_login);
         TextView tvRegister = findViewById(R.id.txt_register);
 
+        handleSSLHandshake();
+        
         UserPreference mUserPreference = new UserPreference(this);
         userModel = mUserPreference.getUser();
         Log.d("tag", userModel.getUserId().toString());
@@ -153,5 +165,35 @@ public class LoginActivity extends AppCompatActivity {
         //userModel.setUserRole(userRole);
 
         userPreference.setUser(userModel);
+    }
+
+    @SuppressLint("TrulyRandom")
+    public static void handleSSLHandshake() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+            });
+        } catch (Exception ignored) {
+        }
     }
 }
